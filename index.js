dbb48b66476f4d5cb9d9ee1ea3b7f8c65422ea3b
berur4 <berurcaleb@gmail.com>
2024-10-17 15:54:53 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Follow along with the examples here
+// function doSomething(thing) {
+//     console.log(thing);
+//   }
+  
+//   doSomething("anything");
+
+//   function sayHelloTo(firstNAme){
+//     console.log(`Hello, ${firstNAme}!`);
+//   }
+//   sayHelloTo("Berur");
+
+//   function say(greeting, firstNAme){
+//     console.log(`${greeting}, ${firstNAme}!`);
+    
+//   }
+//   say("Berur", "Hello");
+
+// function say(greeting, firstName) {
+//     console.log("firstName: ", firstName);
+//     console.log("greeting: ", greeting);
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+//   say("Caleb", "berur");
+// function add(x, y) {
+//     return x + y;
+//   }
+//   console.log(add(1, 2));
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+//   console.log("Hello", "Caleb");
+  
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
